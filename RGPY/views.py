feae4d5c4b1bdd6cb71e8e3b9e6455912aee0d34
wsqy <1323800521@qq.com>
2017-01-30 15:50:34 +0800

    注册基本完成

--- RGPY/views.py
+++ RGPY/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from RGPY.forms import RegisterForm
+from RGPY.models import Student, Banji
 
 
 # Create your views here.
@@ -8,4 +10,25 @@ def test(request):
 
 
 def register(request):
-    return render(request, "RGPY/register.html")
+    print(request.method)
+    print("-----")
+    if request.method == "POST" and request.POST:
+        print(request.POST)
+        print("-----")
+        # wangluo_banji = Banji.objects.get(id=1)
+        # print(wangluo_banji.id)
+        registerFrom = RegisterForm(request.POST)
+        print(registerFrom)
+        print("-----")
+        if registerFrom.is_valid():
+            clear_data = registerFrom.cleaned_data
+            print(clear_data)
+            print("-----")
+            Student.objects.create_user(
+                username=clear_data['username'],
+                password=clear_data['password'],
+                email=clear_data['email'],
+                phone=clear_data['phone'],
+            )
+            return HttpResponse("注册成功")
+    return render(request, "RGPY/register.html", locals())
