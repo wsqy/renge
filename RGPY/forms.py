262ff1adf0123e65f70164a4c50ce7f4c0e9a466
wsqy <1323800521@qq.com>
2017-01-30 11:17:38 +0800

    新建学生注册的表单

---
+++ RGPY/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from RGPY.models import Student
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ("username", "password", "first_name", "phone", "banji")
