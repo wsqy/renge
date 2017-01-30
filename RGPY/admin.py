4081eb32aba112f3f5ab1f6674db92ae31ea5528
wsqy <1323800521@qq.com>
2017-01-30 11:23:28 +0800

    增加后台注册

--- RGPY/admin.py
+++ RGPY/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from RGPY.models import College, Department, Banji, Stuent
 
 # Register your models here.
+admin.site.register(College)
+admin.site.register(Department)
+admin.site.register(Banji)
+admin.site.register(Stuent)
